c46aa316febfc941f43ecba44ce5b21e67a9b8c8
ErikHolman <lifeinthedayof@gmail.com>
2023-10-18 18:04:50 -0700

    new view

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="theSite">
+    <portfolioHeader />
+    <portfolioContent />
+  </div>
+</template>
+
+<script>
+import PortfolioHeader from '@/components/portfolioHeader.vue';
+import PortfolioContent from '@/components/portfolioContent.vue';
+
+export default {
+  name: 'LandingPage',
+  components: { PortfolioHeader, PortfolioContent },
+  props: {},
+};
+</script>
+
+<style scoped>
+.theSite {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 10px;
+}
+</style>
