5cacab01ffeb00dd60fd9fac253f848833b661c3
ErikHolman <lifeinthedayof@gmail.com>
2023-10-19 12:07:36 -0700

    adds a loader component

---
+++ src/components/ehLoader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ehLoader" v-if="isLoading">
+    <img class="profilePic" src="../assets/profile.jpg" />
+    <span>{{ this.isLoading }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ehLoader',
+  props: { showLoader: Boolean },
+  data() {
+    return {
+      // isLoading: this.showLoader,
+      isLoading: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 6000);
+  },
+};
+</script>
+
+<style scoped>
+.ehLoader {
+}
+
+.profilePic {
+  width: 25%;
+  transform: rotate(0.75turn);
+}
+</style>
