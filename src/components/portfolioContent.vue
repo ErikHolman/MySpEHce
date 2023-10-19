ab22a047e0d3d0c46e7756b88410d94374bf3c30
ErikHolman <lifeinthedayof@gmail.com>
2023-10-18 18:05:15 -0700

    new component for view

---
+++ src/components/portfolioContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="portfolioContent">
+    <span>wow a portfolioContent component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'portfolioContent',
+  props: {},
+};
+</script>
+
+<style scoped>
+.portfolioContent {
+  width: auto;
+  border: 1px solid red;
+}
+</style>
---
+++ src/components/portfolioHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="portfolioHeader">
+    <span>wow a portfolioHeader component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'portfolioHeader',
+  props: {},
+};
+</script>
+
+<style scoped>
+.portfolioHeader {
+  padding: 10px;
+  height: 12%;
+  width: auto;
+  border: 1px solid red;
+}
+</style>
